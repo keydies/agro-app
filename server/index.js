e9cccf1cac25a57c403a23464e9f62058ebdf6bf
keydies <serkeydies@gmail.com>
2022-05-18 15:03:15 +0300

    Add registration on server side

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 
+const authRouter = require('./routes/auth.routes');
+
 const app = express();
 const PORT = config.get('serverPort');
+
+app.use(express.json());
+
+app.use('/api/auth', authRouter);
 
 const runServer = async () => {
 	try {
